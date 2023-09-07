b768bb825f58a41c18cde7d5ea0451acf6a8c480
YHamers <50840425+YHamers@users.noreply.github.com>
2023-09-07 11:55:34 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,119 @@
+//https://medium.com/@kj_schmidt/making-a-simple-scatter-plot-with-d3js-58cc894d7c97#:~:text=Making%20a%20simple%20scatter%20plot%20with%20d3.js%201,data%20points%3A%20var%20path%20%3D%20svg.selectAll%20%28%22dot%22%29%20\
+
+
+data = [{
+    date: 2009,
+    wage: 7.25
+}, {
+    date: 2008,
+    wage: 6.55
+}, {
+    date: 2007,
+    wage: 5.85
+}, {
+    date: 1997,
+    wage: 5.15
+}, {
+    date: 1996,
+    wage: 4.75
+}, {
+    date: 1991,
+    wage: 4.25
+}, {
+    date: 1981,
+    wage: 3.35
+}, {
+    date: 1980,
+    wage: 3.10
+}, {
+    date: 1979,
+    wage: 2.90
+}, {
+    date: 1978,
+    wage: 2.65
+}]
+
+
+var margin = {
+    top: 20,
+    right: 20,
+    bottom: 30,
+    left: 40
+}
+width = 700 - margin.left - margin.right;
+height = 500 - margin.top - margin.bottom;
+
+
+// format the data
+data.forEach(function (d) {
+    parseDate = d3.timeParse("%Y");
+    d.date = parseDate(d.date);
+    d.wage = +d.wage;
+});
+//sort the data by date
+data.sort(function (a, b) {
+    console.log(a.date)
+    return a.date - b.date;
+});
+
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
+// Scale the range of the data
+x.domain(d3.extent(data, function (d) {
+     return d.date;
+}));
+y.domain([0, d3.max(data, function (d) {
+     return d.wage;
+})]);
+
+
+var valueline = d3.line()
+     .x(function (d) {
+          return x(d.date);
+     })
+     .y(function (d) {
+          return y(d.wage);
+     });
+
+
+
+     var svg = d3.select("#scatter").append("svg")
+     .attr("width", width + margin.left + margin.right)
+     .attr("height", height + margin.top + margin.bottom)
+     .append("g")
+     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+     svg.append("path")
+     .data([data])
+     .attr("class", "line")
+     .attr("d", valueline)
+     //styling:
+     .attr("stroke", "#32CD32")
+     .attr("stroke-width", 2)
+     .attr("fill", "#FFFFFF");
+
+     var path = svg.selectAll("dot")
+     .data(data)
+     .enter().append("circle")
+     .attr("r", 5)
+     .attr("cx", function (d) {
+           return x(d.date);
+     })
+     .attr("cy", function (d) {
+          return y(d.wage);
+     })
+     .attr("stroke", "#32CD32")
+     .attr("stroke-width", 1.5)
+     .attr("fill", "#FFFFFF");
+
+
+
+     svg.append("g")
+     .attr("transform", "translate(0," + height + ")")
+     .call(d3.axisBottom(x));
+svg.append("g")
+     .call(d3.axisLeft(y).tickFormat(function (d) {
+          return "$" + d3.format(".2f")(d)
+     }));
